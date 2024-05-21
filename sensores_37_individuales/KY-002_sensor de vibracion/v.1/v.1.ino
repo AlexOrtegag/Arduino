56d8887c2ab04093f979bbdac4e4a77105a7890d
AlejandroOrtega <alejandroortega542@gmail.com>
2024-05-20 22:49:58 -0600

    se agrega sensor de vibracion

---
+++ sensores_37_individuales/KY-002_sensor de vibracion/v.1/v.1.ino
@@ -0,0 +1,20 @@
+//para encender un LED cuando el sensor
+//detecta un golpe o vibracion fuerte escribiendo de forma directa en el LED.
+//El sensor brinda un nivel logico bajo cuando detecta la vibracion.
+
+int sensor=2;// pin S de modulo a pin 2
+int led=3;// anodo de LED a pin 3
+int estado;// variable almacena valor leido de pin 2
+
+void setup()
+{
+  pinMode(sensor,INPUT);
+  pinMode(led,OUTPUT);
+}
+
+void loop()
+{
+  estado=digitalRead(sensor);// lee valor de pin 2
+  digitalWrite(led,!estado);// escribe en LED el valor opuesto del leido
+}
+
---
+++ sensores_37_individuales/KY-002_sensor de vibracion/v_2/v_2.ino
@@ -0,0 +1,21 @@
+int sensor=2;// pin S de modulo a pin 2
+int led=3;// anodo de LED a pin 3
+int estado;// variable almacena valor leido de pin 2
+
+void setup()
+{
+  pinMode(sensor,INPUT);
+  pinMode(led,OUTPUT);
+}
+
+void loop()
+{
+  estado=digitalRead(sensor);// lee valor de pin 2
+  if(estado==LOW)// si se detecta vibracion
+  {
+    digitalWrite(led,HIGH);// enciende LED
+    delay(500);// demora de medio segundo para visualizar bien el LED encendido
+  }
+  digitalWrite(led,LOW);// apaga LED
+}
+
