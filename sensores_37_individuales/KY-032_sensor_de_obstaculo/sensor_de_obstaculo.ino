c3a5c7f8b0edd5f11301a89ffcc6fc6954c12217
AlejandroOrtega <alejandroortega542@gmail.com>
2024-05-21 22:38:04 -0600

    sensor de obstaculo

---
+++ sensores_37_individuales/KY-032_sensor_de_obstaculo/sensor_de_obstaculo.ino
@@ -0,0 +1,16 @@
+  
+const int LED = 13;
+const int SENSOR = 2;
+int valor;
+
+void setup(){
+  pinMode(LED, OUTPUT);
+  pinMode(SENSOR, INPUT);  
+}
+
+void loop(){
+  valor = digitalRead(SENSOR);
+  digitalWrite(LED, !valor);
+
+  delay(500);
+}
